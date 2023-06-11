ef7398bed98eb8cdd1774b340efa1a584fc70a05
DINESH <109014495+dinesh4343@users.noreply.github.com>
2023-06-11 12:14:31 +0530

    push

--- register_script.js
+++ register_script.js
@@ -1,10 +1,10 @@
-
 function storeData() {
   const name = document.getElementById("name").value;
   const age = document.getElementById("age").value;
   const mobileNo = document.getElementById("mobile-no").value;
   const address = document.getElementById("address").value;
   const gender = document.querySelector('input[name="gender"]:checked').value;
+  document.getElementById("loading-overlay").style.display = "block";
 
   console.log("Name:", name);
   console.log("Age:", age);
@@ -14,12 +14,14 @@ function storeData() {
 
   realtimeDB.ref("data").push({
     name: name,
+    gender: gender,
     age: age,
     mobileNo: mobileNo,
-    address: address,
-    gender: gender
+    address: address
+    
   })
   .then(() => {
+    document.getElementById("loading-overlay").style.display = "none";
     console.log("Data stored successfully in Realtime Database.");
 
     // Clear input fields
@@ -33,47 +35,58 @@ function storeData() {
   })
   .catch((error) => {
     console.error("Error storing data:", error);
+    document.getElementById("loading-overlay").style.display = "none";
   });
 
-   const email = document.getElementById("email").value;
+  const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
-    
+
   auth.createUserWithEmailAndPassword(email, password)
     .then((userCredential) => {
       console.log("User signed up:", userCredential);
       alert("You have successfully signed up!");
+      
+      // Clear input fields
+      document.getElementById("email").value = "";
+      document.getElementById("password").value = "";
+
+      // Redirect to login page
+      window.location.href = "index.html";
     })
     .catch((error) => {
       console.error("Error signing up:", error);
       alert("Error signing up. Please try again.");
+      document.getElementById("email").value = "";
+      document.getElementById("password").value = "";
     });
 }
 
-function signUp() {
- 
-}
 
-function toggleForms() {
-  var registerForm = document.getElementById("register_form");
-  var signupForm = document.getElementById("signup_form");
-  registerForm.classList.toggle("hidden");
-  signupForm.classList.toggle("hidden");
-}
 
-function toggleBack() {
-  var registerForm = document.getElementById("register_form");
-  var signupForm = document.getElementById("signup_form");
-  registerForm.classList.toggle("hidden");
-  signupForm.classList.toggle("hidden");
-}
+    function toggleForms() {
+      var registerForm = document.getElementById("register_form");
+      var signupForm = document.getElementById("signup_form");
+      registerForm.classList.toggle("hidden");
+      signupForm.classList.toggle("hidden");
+    }
+    
+    function toggleBack() {
+      var registerForm = document.getElementById("register_form");
+      var signupForm = document.getElementById("signup_form");
+      registerForm.classList.toggle("hidden");
+      signupForm.classList.toggle("hidden");
+    }
 
-const menuToggle = document.querySelector('#menuToggle input');
+
+
+
+    const menuToggle = document.querySelector('#menuToggle input');
 
 // Add click event listener to the document
 document.addEventListener('click', function(event) {
-// Check if the clicked element is inside the menu or menu toggle
-if (!event.target.closest('#menu') && !event.target.closest('#menuToggle')) {
-// Uncheck the menu toggle checkbox to close the menu
-menuToggle.checked = false;
-}
+  // Check if the clicked element is inside the menu or menu toggle
+  if (!event.target.closest('#menu') && !event.target.closest('#menuToggle')) {
+    // Uncheck the menu toggle checkbox to close the menu
+    menuToggle.checked = false;
+  }
 });
