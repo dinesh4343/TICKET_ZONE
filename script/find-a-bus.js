8fc9a14a69d18f8f8b6aba66ba610a1ff9481116
DINESH <109014495+dinesh4343@users.noreply.github.com>
2023-09-15 15:38:57 +0530

    version2.0

---
+++ script/find-a-bus.js
@@ -0,0 +1,53 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchButton = document.getElementById("searchButton");
+    const busNumberInput = document.getElementById("busNumber");
+    const routeList = document.getElementById("routeList");
+
+    // Add an event listener for the "keydown" event on the input field
+    busNumberInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault(); // Prevent form submission if inside a form
+            const busNumber = busNumberInput.value;
+            checkBusNumber(busNumber);
+        }
+    });
+
+    searchButton.addEventListener("click", function () {
+        const busNumber = busNumberInput.value;
+        checkBusNumber(busNumber);
+    });
+
+    function checkBusNumber(busNumber) {
+        $.ajax({
+            url: "lib/bus_stops.csv", 
+            dataType: "text",
+            success: function(data) {
+                let rows = data.split("\n");
+                let foundRows = [];
+                
+                for (let i = 0; i < rows.length; i++) {
+                    let cols = rows[i].split(",");
+                    if (cols[1] === busNumber) {
+                        foundRows.push(cols);
+                    }
+                }
+
+                if (foundRows.length > 0) {
+                    let routeInfo = "";
+
+                    for (let i = 0; i < foundRows.length; i++) {
+                        let start = foundRows[i][3]; // Starting point
+
+                        routeInfo += `<li></strong> ${start}</li>`;
+                    }
+
+                    routeList.innerHTML = routeInfo;
+                } else {
+                    routeList.innerHTML = "Bus number not found.";
+                }
+            }
+        });
+    }
+});
+
