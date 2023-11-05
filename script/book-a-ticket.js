35df40ff8bf66a13926e7e2dd7e3bccb8a7de804
dinesh4343 <dineshpandian008@gmail.com>
2023-11-05 21:10:54 +0530

    book-a-ticket

--- script/book-a-ticket.js
+++ script/book-a-ticket.js
@@ -44,6 +44,34 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
+    
+
+      // Function to go back
+      function goBack() {
+        // You can customize this logic to navigate back or perform other actions
+    
+        searchContainer.style.display = "none";
+        district.style.display = "block";
+        
+        backButton.style.display = "none";
+      }
+
+
+
+
+
+
+
+
+
+
+      function showLoader() {
+        loaderOverlay.style.display = "flex";
+        setTimeout(function () {
+          loaderOverlay.style.display = "none";
+        }, 1000); // 5000 milliseconds = 5 seconds
+      }
+  
 
 
 
@@ -114,16 +142,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-
-      // Function to go back
-      function goBack() {
-        // You can customize this logic to navigate back or perform other actions
     
-        searchContainer.style.display = "none";
-        district.style.display = "block";
-       
-        backButton.style.display = "none";
-      }
 
 
 
@@ -134,11 +153,165 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-      function showLoader() {
-        loaderOverlay.style.display = "flex";
-        setTimeout(function () {
-          loaderOverlay.style.display = "none";
-        }, 1000); // 5000 milliseconds = 5 seconds
-      }
-  
+
+
+
+
+
+
+    
+
+    const searchButton = document.getElementById("searchButton");
+    const searchStopNameInput = document.getElementById("searchStopName");
+    const routeList = document.getElementById("routeList");
+    const originBox = document.querySelector(".box h2:first-child + h4");
+    const destinationBox = document.querySelector(".box1 h2:first-child + h4");
+
+    searchStopNameInput.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            const searchStopName = searchStopNameInput.value;
+            checkStopName(searchStopName);
+        }
     });
+
+    searchButton.addEventListener("click", function () {
+        const searchStopName = searchStopNameInput.value;
+        checkStopName(searchStopName);
+    });
+
+    
+    
+    function checkStopName(searchStopName) {
+    
+    $.ajax({
+        type: "GET",
+    url: "lib/bus_stops.csv",
+    dataType: "text",
+    success: function (data) {
+        let lines = data.split("\n");
+    let foundRoutes = [];
+    for (let i = 1; i < lines.length; i++) {
+    let cols = lines[i].split(",");
+    if (cols.length >= 4 && cols[3] && cols[3].trim() === searchStopName.trim()) {
+        foundRoutes.push(cols);
+    }
+}
+
+            if (foundRoutes.length > 0) {
+                let routeInfo = "";
+              
+                    
+
+
+                for (let i = 0; i < foundRoutes.length; i++) {
+                    $.ajax({
+                        type: "GET",
+                        url: "lib/bus_stops.csv",
+                        dataType: "text",
+                        success: function (data) {
+                            let new_lines = data.split("\n");
+                            let new_foundRoutes = [];
+                            
+
+                    
+
+                            for (let j = 1; j < new_lines.length; j++) {
+                                let new_cols = new_lines[j].split(",");
+
+                                if (new_cols[1] && foundRoutes[i] && foundRoutes[i][1] && new_cols[1] === foundRoutes[i][1]) {
+                                    new_foundRoutes.push(new_cols[3]);
+                                }
+                            }
+
+                            
+
+                            let start = new_foundRoutes;
+                            let zero=foundRoutes[i][1];
+
+                            routeInfo += `<li>
+                            
+                            <div class="timeline--circle"> <i></i></div>
+                            <div class="timeline--description">${zero} -> ${start}</div> </li>`;
+                            routeList.innerHTML = routeInfo;
+
+                        },
+                        error: function (error) {
+                            console.error("Error fetching data:", error);
+                        }
+                    });
+                }
+
+             
+            } else {
+                routeList.innerHTML = "<li>Stop name not found.</li>";
+            }
+
+
+        },
+        error: function () {
+            // Handle the case where there is an error in the AJAX request
+            routeList.innerHTML = "<li>Error loading data.</li>";
+
+        }
+    });
+}
+
+
+
+}); 
+
+
+
+
+function suggestStops() {
+  let input = $('#searchStopName').val().toUpperCase();
+  if (input.length >= 2) {
+      $.ajax({
+          type: "GET",
+          url: "lib/bus_stops.csv", 
+          dataType: "text",
+          success: function (data) {
+              let lines = data.split("\n");
+              let uniqueSuggestions = new Set();
+
+              for (let i = 1; i < lines.length; i++) {
+                  let cols = lines[i].split(",");
+                  if (cols.length >= 4 && cols[3] && cols[3].toUpperCase().includes(input)) {
+                      uniqueSuggestions.add(cols[3]);
+                  }
+              }
+
+              displaySuggestions(Array.from(uniqueSuggestions));
+          },
+          error: function (error) {
+              console.error("Error fetching data:", error);
+          }
+      });
+  } else {
+      clearSuggestions();
+  }
+}
+
+function displaySuggestions(suggestions) {
+  let suggestionsList = $('#suggestions');
+  suggestionsList.empty();
+
+  for (let i = 0; i < suggestions.length; i++) {
+      let suggestionItem = $('<li>' + suggestions[i] + '</li>');
+      suggestionItem.click(function () {
+          $('#searchStopName').val(suggestions[i]);
+          clearSuggestions();
+      });
+      suggestionsList.append(suggestionItem);
+  }
+  suggestionsList.show(); 
+}
+
+function clearSuggestions() {
+  $('#suggestions').empty().hide();
+}
+
+
+
+
